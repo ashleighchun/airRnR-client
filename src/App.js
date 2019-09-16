56181ec0ef170491e462e41b1599e42f650f7f96
Ashleigh Chun <ashleigh.chun@gmail.com>
2019-09-16 01:26:53 -0700

    add trips container to main page

--- src/App.js
+++ src/App.js
@@ -3,36 +3,19 @@ import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux'
 import { getTrips } from './actions/trips'
-import TripForm from './components/TripForm'
+import TripsContainer from './containers/TripsContainer'
 
-//componentdidmount will cause it to fire off first get trips when the component mounted
-class App extends Component {
-  componentDidMount(){
-    this.props.getTrips()
-  }
-  render(){
-    const trips = this.props.trips.map((trip, i) => <li key={i}> {trip.name} - {trip.completed ? "Completed" : "Coming Up"}</li>);
+
+class App extends React.Component {
+
+render() {
     return (
       <div className="App">
-        <h2>Create Trip</h2>
-        <TripForm />
-        <hr />
-        <h2>Trip List</h2>
-        <ul>
-          {this.props.loading ? <h3>Loading...</h3> : trips }
-        </ul>
+        <TripsContainer/>
       </div>
     );
   }
 }
 
 
-//***map global state to the props of this specific component
-const mapStateToProps = (state) => {
-  console.log("I am state", state)
-  return {
-    trips: state.tripReducer.trips,
-    loading: state.tripReducer.loading
-  }
-}
-export default connect(mapStateToProps, { getTrips })(App);
+export default App;
