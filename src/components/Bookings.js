88e22d721d8dba458291ce502de56c9cbd159f6b
Ashleigh Chun <ashleigh.chun@gmail.com>
2019-09-23 14:33:41 -0700

    add bookings map function

--- src/components/Bookings.js
+++ src/components/Bookings.js
@@ -1,1 +1,16 @@
 import React from 'react'
+
+const Bookings = (props) => {
+
+  consoe.log(props.bookings)
+
+  return (
+    <div>
+      {props.bookings && props.bookings.map(transaction =>
+        <li key={booking.id}> {booking.booking_type} - {booking.cost} - {booking.start_date} - {booking.end_date}</li>
+      )}
+    </div>
+  )
+}
+
+export default Bookings
