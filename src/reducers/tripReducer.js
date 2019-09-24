bbacb961dc038f6cda927005d247745128a10f93
Ashleigh Chun <ashleigh.chun@gmail.com>
2019-09-24 10:39:12 -0700

    build out rest of async reducers for trips

--- src/reducers/tripReducer.js
+++ src/reducers/tripReducer.js
@@ -1,12 +1,36 @@
 export default function tripReducer(state = {trips: []}, action) {
-  debugger;
+
   switch (action.type) {
     case 'FETCH_TRIPS':
       return {trips: action.payload}
     case 'ADD_TRIP':
       return {...state, trips: [...state.trips, action.payload]}
     case 'ADD_BOOKING':
-      return {}
+      let trips = state.trips.map(trip => {
+        if (trip.id === action.payload.id) {
+          return action.payload
+        } else {
+          return trip
+        }
+      })
+      return {...state, trips: trips}
+    case 'DELETE_BOOKING':
+      let tripsTwo = state.trips.map(trip => {
+        if (trip.id === action.payload.id) {
+          return action.payload.id
+        } else {
+          return trip
+        }
+      })
+    case 'EDIT_TRIP':
+      let tripsThree = state.trips.map(trip => {
+        if (trip.id === action.payload.id) {
+          return action.payload
+        } else {
+          return trip
+        }
+      })
+      return {...state, trips: tripsThree}
     default:
       return state
   }
