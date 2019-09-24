5e2bac2ec7c692297ad4d61392e98d3d68aa8bdd
Ashleigh Chun <ashleigh.chun@gmail.com>
2019-09-24 10:13:42 -0700

    delete reducer index

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import tripReducer from './tripReducer'
-
-const rootReducer = combineReducers({
-  tripReducer
-  //bookingReducer
-})
-
-export default rootReducer;
