6741fedbf8a403c30c1f83260d684da0dd13d2ab
Ashleigh Chun <ashleigh.chun@gmail.com>
2019-09-24 21:34:44 -0700

    add formatting and modify index

--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware, compose} from 'redux'
-import thunk from 'redux-thunk'
-import { Provider } from 'react-redux'
-import {BrowserRouter as Router} from 'react-router-dom'
-import tripReducer from './reducers/tripReducer'
-
-
+import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers'
+import registerServiceWorker from './registerServiceWorker';
+import thunk from 'redux-thunk';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-let store = createStore(tripReducer, composeEnhancers(applyMiddleware(thunk)))
+const store=createStore(rootReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <App/>
-    </Router>
-  </Provider>,
-  document.getElementById('root'));
+  <Provider store={store} >
+    <App />
+  </Provider>
+  , document.getElementById('root'));
+registerServiceWorker();
