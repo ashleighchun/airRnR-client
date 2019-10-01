600cc19c0517fc29a9c08d7f4f05d8b482039117
Ashleigh Chun <ashleigh.chun@gmail.com>
2019-10-01 02:31:03 -0400

    restyle components

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home!</h1>
-        </div>
+      <div className="home">
+    <br />
+    <h1>Welcome to AirRnR!</h1>
+    <h3>keep track of the plans you have booked for your next trip!</h3>
+    <img src="https://www.destinasian.com/travel/wp-content/uploads/150324_flights-hero-image_1330x742-3.jpg" alt="fly away" className="img-responsive"/>
+  </div>
     )
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,18 +2,45 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const link={
-  color: 'blue',
-  textDecoration: 'underline',
-  padding: 5,
-  margin: 5
+  width: '90px',
+   padding: '10px',
+   margin: '0',
+   background: '#9c988f',
+   textDecoration: 'none',
+   fontFamily: 'Garamond',
+   fontSize: 12,
+   color: 'white'
 }
 const NavBar = () => {
   return (
     <div className="navbar">
-    <NavLink to="/" activeStyle={link }>Home</NavLink>
-    <NavLink to="/trips" activeStyle={link}>Trips</NavLink>
-    <NavLink to="/bookings" activeStyle={link}>Bookings</NavLink>
-    <NavLink to="/about" activeStyle={link}>About</NavLink>
+
+    <NavLink to='/'
+        exact
+        style={link}
+        activeStyle={{
+          background: 'black'
+        }}
+      >Home</NavLink>
+
+    <NavLink to='/books'
+         exact
+         style={link}
+         activeStyle={{
+            background: 'black'
+        }}
+     >Your Bookings</NavLink>
+
+    <NavLink
+         to='/books/new'
+         exact
+         style={link}
+         activeStyle={{
+            background: 'black'
+        }}
+     >Add a Booking</NavLink>
+
+   </div>
     </div>
   );
 };
